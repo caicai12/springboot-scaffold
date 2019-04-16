0dc4baf73121428c115708537ccce361c1c58c6a
zhouhui5116 <zhouhui5116@163.com>
2019-04-16 11:23:46 +0800

    Create springboot-rabbitmq

---
+++ springboot-rabbitmq/src/main/java/com/zhh/config/RabbitMQConfig.java
@@ -0,0 +1,22 @@
+package com.zhh.config;
+
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * @Description: RabbitMQ配置类
+ * @Author: zhouhui
+ * @Version: V1.0
+ * @Date: 2019/4/27 17:54
+ */
+@Configuration
+public class RabbitMQConfig {
+    // 定义转换器，将数据序列化为JSON格式传输
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+}
